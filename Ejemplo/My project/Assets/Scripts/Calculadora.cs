c7f35867f75eb57f2b155e58a7e8b03bc36e8ab8
zafiro333 <gabrielle.sandoval36@my.unitec.edu.mx>
2024-05-28 08:17:35 -0600

    Ya casi, falta arreglar división y multiplicaci+on

--- Ejemplo/My project/Assets/Scripts/Calculadora.cs
+++ Ejemplo/My project/Assets/Scripts/Calculadora.cs
@@ -10,15 +10,15 @@ public class Calculadora : MonoBehaviour
     public TMP_InputField segundoNumero;
     public TextMeshProUGUI resultadoText; // Campo de texto para mostrar el resultado
     public Button calcularButton; // Botón para calcular
+    public TMP_Dropdown operacionDropdown; // Dropdown para seleccionar la operación
 
-    // Métodos
     void Start()
     {
-        // Asigna el método Suma al botón
-        calcularButton.onClick.AddListener(Suma);
+        // Asigna el método Calcular al botón
+        calcularButton.onClick.AddListener(Calcular);
     }
 
-    public void Suma()
+    public void Calcular()
     {
         int numero1;
         int numero2;
@@ -26,7 +26,34 @@ public class Calculadora : MonoBehaviour
         // Validar y convertir los valores de los campos de entrada
         if (int.TryParse(primerNumero.text, out numero1) && int.TryParse(segundoNumero.text, out numero2))
         {
-            int resultado = numero1 + numero2;
+            int resultado = 0;
+            switch (operacionDropdown.value)
+            {
+                case 0: // Suma
+                    resultado = numero1 + numero2;
+                    break;
+                case 1: // Resta
+                    resultado = numero1 - numero2;
+                    break;
+                case 2: // Multiplicación
+                    resultado = numero1 * numero2;
+                    break;
+                case 3: // División
+                    if (numero2 != 0)
+                    {
+                        resultado = numero1 / numero2;
+                    }
+                    else
+                    {
+                        Debug.LogError("No se puede dividir por cero.");
+                        resultadoText.text = "Error: División por cero";
+                        return;
+                    }
+                    break;
+                default:
+                    Debug.LogError("Operación no soportada.");
+                    return;
+            }
             resultadoText.text = resultado.ToString(); // Mostrar el resultado en el campo de texto
         }
         else
@@ -35,7 +62,6 @@ public class Calculadora : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
