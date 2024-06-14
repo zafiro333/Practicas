c1d78078b467ebf582f5efb5fdb12097989fde62
zafiro333 <gabrielle.sandoval36@my.unitec.edu.mx>
2024-06-14 11:32:27 -0600

    Acomodando Pastel

---
+++ Ejemplo/My project/Assets/Scripts/FlavorController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FlavorController : MonoBehaviour
+{
+    private TMP_Dropdown colorOptions;
+
+    void Start()
+    {
+       colorOptions = GetComponent<TMP_Dropdown>();
+
+    }
+
+    
+}
