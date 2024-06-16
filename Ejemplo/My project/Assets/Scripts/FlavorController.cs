22c52406b58f72c3c32bd7e1b895cc0204f7e343
zafiro333 <gabrielle.sandoval36@my.unitec.edu.mx>
2024-06-15 18:23:34 -0600

    La base pastel funciona

--- Ejemplo/My project/Assets/Scripts/FlavorController.cs
+++ Ejemplo/My project/Assets/Scripts/FlavorController.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class FlavorController : MonoBehaviour
 {
     private TMP_Dropdown colorOptions;
+    public Image pastel;
+    public Sprite [] flavorsBase;
 
     void Start()
     {
        colorOptions = GetComponent<TMP_Dropdown>();
+       pastel = GameObject.Find("Pastel").GetComponent<Image>();
 
     }
+    public void ChangeFlavor()
+    {
+        switch (colorOptions.value)
+        {
+           case 0:
+           pastel.sprite = flavorsBase[0];
+           break;
+           case 1:
+           pastel.sprite = flavorsBase[1];
+           break;
+           case 2:
+           pastel.sprite = flavorsBase[2];
+           break;
+        }
+       
+    }
 
-    
+
 }
