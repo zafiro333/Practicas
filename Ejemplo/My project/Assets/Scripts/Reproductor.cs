75ba938fc02bee9635693cac26776d6552f54aa2
zafiro333 <gabrielle.sandoval36@my.unitec.edu.mx>
2024-06-25 11:46:51 -0600

    Reproductor funcional

--- Ejemplo/My project/Assets/Scripts/Reproductor.cs
+++ Ejemplo/My project/Assets/Scripts/Reproductor.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Reproductor : MonoBehaviour
 {
+    public static Reproductor instance;
     public AudioSource repAudio;
     public List<AudioClip> album_01 = new List<AudioClip>();
     public Transform trackContainer;
     public GameObject trackObj;
     public int currentTrack;
+    public bool album01;
+    public Slider volumecontrol;
 
-    
+    private void Awake()
+    {
+        instance= this;
+    }
+
+
     void Start()
     {
        for (int i = 0; i < album_01.Count; i++)
@@ -18,6 +27,43 @@ public class Reproductor : MonoBehaviour
             Instantiate(trackObj, trackContainer);
             trackObj.GetComponent<Track>().clip = album_01[i];
             trackObj.GetComponent<Track>().idTrack = i;
+        }
+    }
+
+    private void Update()
+    {
+        repAudio.volume = volumecontrol.value;
+    }
+
+    public void Foward()
+    {
+        if (currentTrack < album_01.Count -1)
+        {
+            currentTrack++;
+            repAudio.clip = album_01[currentTrack];
+            repAudio.Play();
+        }
+        else
+        {
+            currentTrack = 0;
+            repAudio.clip = album_01[0];
+            repAudio.Play();
+        }
+    }
+
+    public void Backward()
+    {
+        if(currentTrack > 0)
+        {
+            currentTrack--;
+            repAudio.clip =album_01[currentTrack];
+            repAudio.Play();
+        }
+        else
+        {
+            currentTrack = album_01.Count -1;
+            repAudio.clip = album_01[currentTrack];
+            repAudio.Play();
         }
     }
 }
--- Ejemplo/My project/Assets/Scripts/Track.cs
+++ Ejemplo/My project/Assets/Scripts/Track.cs
@@ -1,16 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
-using Unity.VisualScripting;
 
 
 public class Track : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip clip;
-    public Button thisButton;
     public TMP_Text clipName;
     public int idTrack;
 
@@ -35,6 +32,7 @@ public class Track : MonoBehaviour
     {
         audioSource.clip = clip;
         audioSource.Play();
+        Reproductor.instance.currentTrack = idTrack;
     }    
    
 }
